9d420ccb56290ee7d6387eb5281ec329a9d31ab1
karthikkode <karthiktumusai@gmail.com>
2024-12-21 00:22:09 +0530

    fix fe

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173, // Use the PORT environment variable or fallback to 4173
+    host: true, // Expose the server to external access
+  },
   plugins: [react()],
   resolve: {
     alias: {
